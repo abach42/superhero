a65777a8ec7e01b810da974aad4c6501093960c9
abach42 <arend.maubach@gmx.net>
2024-04-16 21:34:16 +0200

    liquibase as database fixture service

--- src/test/java/com/abach42/superhero/repository/SuperheroRepositoryTest.java
+++ src/test/java/com/abach42/superhero/repository/SuperheroRepositoryTest.java
@@ -3,37 +3,25 @@ package com.abach42.superhero.repository;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Description;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
+import org.springframework.context.annotation.Import;
+
+import com.abach42.superhero.configuration.TestContainerConfiguration;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@Testcontainers
+@Import(TestContainerConfiguration.class)
 public class SuperheroRepositoryTest {
-    private final Logger log = LoggerFactory.getLogger(getClass());
 
     @Autowired
     private SuperheroRepository superheroRepository;
-
-    
-    @Container
-    @ServiceConnection
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
-            "postgres:latest"
-    );
 
     @Test
     @Description("find a superhero on find all")
     void testFindAll() {
         assertThat(superheroRepository.count()).isGreaterThan(0L);
     }
-
 }
