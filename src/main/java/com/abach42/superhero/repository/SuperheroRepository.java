09ce17b705a1d412eacea70fb33c932991b9bd6f
abach42 <arend.maubach@gmx.net>
2024-04-16 11:25:35 +0200

    [WIP] Superhero DTO

--- src/main/java/com/abach42/superhero/repository/SuperheroRepository.java
+++ src/main/java/com/abach42/superhero/repository/SuperheroRepository.java
@@ -8,7 +8,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.abach42.superhero.entity.Superhero;
 
 public interface SuperheroRepository extends JpaRepository<Superhero, Long>{
-    List<Superhero> findAll();
-    Optional<Superhero> findById(Long id);
+    List<Superhero> findAll(); //todo not necessary
+    Optional<Superhero> findById(Long id); //todo not necessary
     //todo sort, pagable
+    //todo not deleted
 }
