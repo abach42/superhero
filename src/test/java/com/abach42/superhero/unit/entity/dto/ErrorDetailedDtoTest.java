25a4caf1413310435e50d8fddeaf74e336908ab1
abach42 <arend.maubach@gmx.net>
2024-04-23 09:58:41 +0200

    test dto

---
+++ src/test/java/com/abach42/superhero/unit/entity/dto/ErrorDetailedDtoTest.java
@@ -0,0 +1,34 @@
+package com.abach42.superhero.unit.entity.dto;
+
+import org.junit.jupiter.api.Test;
+
+import com.abach42.superhero.entity.dto.ErrorDetailedDto;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ErrorDetailedDtoTest {
+
+    @Test
+    public void testErrorDetailedDto() {
+        // Arrange
+        Integer status = 400;
+        String error = "Bad Request";
+        String message = "Validation failed";
+        String path = "/api/v1/resource";
+        String field = "name";
+        String validationMessage = "Name is required";
+
+        // Act
+        ErrorDetailedDto errorDetailedDto = new ErrorDetailedDto(status, error, message, path);
+        errorDetailedDto.addValidationError(field, validationMessage);
+
+        // Assert
+        assertEquals(status, errorDetailedDto.getStatus());
+        assertEquals(error, errorDetailedDto.getError());
+        assertEquals(message, errorDetailedDto.getMessage());
+        assertEquals(path, errorDetailedDto.getPath());
+
+        var errors = errorDetailedDto.getErrors();
+        assertEquals(1, errors.size());
+    }
+}
