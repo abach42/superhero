75bfe5ab72213b170729aea991a4ff131a8ad4a1
abach42 <arend.maubach@gmx.net>
2025-07-08 21:54:09 +0200

    update start scripts and docu

---
+++ bin/dev/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Script to run Maven with specified parameters
+
+# Navigate to the directory containing the mvnw file
+cd "$(dirname "$0")/.." || exit
+
+source ./bin/init/init.sh
+
+. "$(dirname "$0")/init/maven-cmd.sh"
+
+echo "--------------------------------------------------"
+echo " 🪴  starting spring on default profile, skipping tests"
+echo "--------------------------------------------------"
+
+# you can add -PmyProfile 
+$mvn clean spring-boot:run \
+    -Dspring-boot.run.jvmArguments="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005"
