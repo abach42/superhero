fa2b8c83401a07c46aea0a8bb2de86c44960a53a
abach42 <arend.maubach@gmx.net>
2026-02-06 22:30:02 +0100

    update skill profiles

--- src/main/resources/oci/db/create.sql
+++ src/main/resources/oci/db/create.sql
@@ -37,23 +37,23 @@ CREATE TABLE skill_profile (
 );
 
 INSERT INTO skill (skill_name) VALUES
-('strength'), -- Stärke
-('speed'), -- Schnelligkeit
-('self_confidence'), -- Selbstvertrauen
-('patience'), -- Geduld
-('kindness'), -- Freundlichkeit
-('courage'), -- Mut
-('compassion'), -- Mitgefühl
-('loyalty'), -- Treue
-('generosity'), -- Großzügigkeit
-('empathy'), -- Empathie
-('wisdom'), -- Weisheit
-('creativity'), -- Kreativität
-('peacekeeping'), -- Friedensstifter
-('charity'), -- Nächstenliebe
-('gentleness'), -- Sanftmut
-('self_control'), -- Selbstbeherrschung
-('good_looks'); -- Gut-Aussehen
+('strength'), -- 1 Stärke
+('speed'), -- 2 Schnelligkeit
+('self_confidence'), -- 3 Selbstvertrauen
+('patience'), -- 4 Geduld
+('kindness'), -- 5 Freundlichkeit
+('courage'), -- 6 Mut
+('compassion'), -- 7 Mitgefühl
+('loyalty'), -- 8 Treue
+('generosity'), -- 9 Großzügigkeit
+('empathy'), -- 10 Empathie
+('wisdom'), -- 11 Weisheit
+('creativity'), -- 12 Kreativität
+('peacekeeping'), -- 13 Friedensstifter
+('charity'), -- 14 Nächstenliebe
+('gentleness'), -- 15 Sanftmut
+('self_control'), -- 16 Selbstbeherrschung
+('good_looks'); -- 17 Gut-Aussehen
 
 INSERT INTO application_user (email, password, role)
 VALUES
@@ -80,7 +80,7 @@ VALUES
 (2, 'Captain Courageous', 'Chris Courage', '1980-05-15', 0, 'Defender of Justice', 'Chris Courage was a regular citizen until a fateful accident imbued him with superhuman courage.'),
 (3, 'Speedster', 'Samantha Swift', '1992-08-21', 1, 'Delivery Driver', 'Samantha Swift gained the ability to move at incredible speeds after being struck by lightning.'),
 (4, 'The Kindness Knight', 'Kevin Knight', '1975-02-10', 0, 'Social Worker', 'Kevin Knight was inspired to become a hero after witnessing acts of kindness in his community.'),
-(5, 'Doctor Empathy', 'Emma Empath', '1988-11-03', 1, 'Psychologist', 'Emma Empath developed the power to sense and understand the emotions of others during a medical experiment.'),
+(5, 'Doctor Empathy', 'Emma Empath', '1988-11-03', 1, 'Psychologist', 'Emma Empath developed the power to sense and understand the emotions of others during a medical research project to rescue little puppies.'),
 (6, 'The Wise Owl', 'Oliver Wisdom', '1963-09-28', 0, 'Retired Teacher', 'Oliver Wisdom''s wisdom is legendary, and he uses his knowledge to guide and mentor other heroes.'),
 (7, 'Creative Genius', 'Cassandra Creative', '1995-04-17', 1, 'Artist', 'Cassandra Creative''s imaginative mind allows her to come up with ingenious solutions to any problem.'),
 (8, 'The Gentle Giant', 'Gary Gentle', '1978-07-09', 0, 'Construction Worker', 'Gary Gentle possesses superhuman strength, but he uses it with care and gentleness to help others.'),
@@ -93,40 +93,39 @@ VALUES
 (15, 'The Empathetic Eagle', 'Ella Empathy', '1984-09-12', 1, 'Therapist', 'Ella Empathy can feel the pain of others and uses her powers to heal both physically and emotionally.'),
 (16, 'The Wise Wizard', 'Walter Wisdom', '1960-11-20', 0, 'Librarian', 'Walter Wisdom''s vast knowledge of ancient texts and magical artifacts makes him a formidable ally in the fight against evil.');
 
-INSERT INTO skill_profile (superhero_id, skill_id, intensity)
-VALUES
--- Insert skill profiles for 'The Administrator'
-(1, 1, 4), (1, 7, 2), (1, 11, 5), (1, 15, 3), (1, 5, 4), (1, 10, 1),
--- Insert skill profiles for 'Captain Courageous'
-(2, 6, 3), (2, 7, 5), (2, 12, 2), (2, 16, 4), (2, 1, 3), (2, 11, 5), (2, 14, 2),
--- Insert skill profiles for 'Speedster'
-(3, 2, 5), (3, 10, 4), (3, 15, 3), (3, 16, 2), (3, 5, 1), (3, 9, 5), (3, 13, 3),
--- Insert skill profiles for 'The Kindness Knight'
-(4, 5, 5), (4, 7, 4), (4, 9, 3), (4, 15, 2), (4, 1, 4), (4, 3, 3), (4, 8, 5), (4, 14, 1),
--- Insert skill profiles for 'Doctor Empathy'
-(5, 7, 5), (5, 9, 4), (5, 10, 3), (5, 11, 2), (5, 2, 5), (5, 4, 3), (5, 12, 4), (5, 15, 1),
--- Insert skill profiles for 'The Wise Owl'
-(6, 6, 5), (6, 10, 4), (6, 11, 3), (6, 16, 2), (6, 3, 5), (6, 8, 2), (6, 13, 4), (6, 14, 3),
--- Insert skill profiles for 'Creative Genius'
-(7, 11, 5), (7, 12, 4), (7, 13, 3), (7, 16, 2), (7, 2, 4), (7, 4, 3), (7, 5, 2), (7, 8, 5),
--- Insert skill profiles for 'The Gentle Giant'
-(8, 1, 5), (8, 7, 4), (8, 9, 3), (8, 15, 2), (8, 3, 5), (8, 6, 3), (8, 10, 4), (8, 14, 1),
--- Insert skill profiles for 'Lady Loyalty'
-(9, 7, 5), (9, 9, 4), (9, 11, 3), (9, 16, 2), (9, 1, 3), (9, 4, 4), (9, 12, 5), (9, 15, 1),
--- Insert skill profiles for 'The Peacekeeper'
-(10, 10, 5), (10, 11, 4), (10, 13, 3), (10, 15, 2), (10, 2, 5), (10, 3, 3), (10, 7, 4), (10, 9, 1),
--- Insert skill profiles for 'The Generous Guardian'
-(11, 9, 5), (11, 11, 4), (11, 12, 3), (11, 16, 2), (11, 1, 2), (11, 5, 3), (11, 13, 4), (11, 14, 1),
--- Insert skill profiles for 'The Courageous Cat'
-(12, 1, 5), (12, 7, 4), (12, 12, 3), (12, 16, 2), (12, 2, 5), (12, 4, 3), (12, 9, 4), (12, 15, 1),
--- Insert skill profiles for 'The Swift Swimmer'
-(13, 2, 5), (13, 11, 4), (13, 12, 3), (13, 16, 2), (13, 6, 5), (13, 8, 3), (13, 9, 4), (13, 15, 1),
--- Insert skill profiles for 'The Kind-hearted Knight'
-(14, 5, 5), (14, 7, 4), (14, 11, 3), (14, 16, 2), (14, 2, 4), (14, 3, 3), (14, 10, 5), (14, 13, 1),
--- Insert skill profiles for 'The Empathetic Eagle'
-(15, 7, 5), (15, 11, 4), (15, 15, 3), (15, 16, 2), (15, 1, 3), (15, 6, 4), (15, 12, 5), (15, 14, 1),
--- Insert skill profiles for 'The Wise Wizard'
-(16, 6, 5), (16, 10, 4), (16, 11, 3), (16, 16, 2), (16, 2, 5), (16, 5, 3), (16, 9, 4), (16, 13, 1);
+INSERT INTO skill_profile (superhero_id, skill_id, intensity) VALUES
+-- 1 The Administrator
+(1,1,2),(1,2,1),(1,3,4),(1,4,5),(1,5,3),(1,6,4),(1,7,3),(1,8,4),(1,9,2),(1,10,3),(1,11,5),(1,12,2),(1,13,4),(1,14,2),(1,15,2),(1,16,5),(1,17,1),
+-- 2 Captain Courageous
+(2,1,4),(2,2,3),(2,3,5),(2,4,3),(2,5,3),(2,6,5),(2,7,4),(2,8,4),(2,9,2),(2,10,3),(2,11,3),(2,12,2),(2,13,5),(2,14,2),(2,15,2),(2,16,4),(2,17,3),
+-- 3 Speedster
+(3,1,2),(3,2,5),(3,3,4),(3,4,2),(3,5,2),(3,6,4),(3,7,2),(3,8,2),(3,9,2),(3,10,3),(3,11,2),(3,12,2),(3,13,2),(3,14,1),(3,15,1),(3,16,3),(3,17,3),
+-- 4 The Kindness Knight
+(4,1,4),(4,2,2),(4,3,4),(4,4,4),(4,5,5),(4,6,3),(4,7,5),(4,8,5),(4,9,4),(4,10,3),(4,11,3),(4,12,2),(4,13,4),(4,14,5),(4,15,4),(4,16,3),(4,17,2),
+-- 5 Doctor Empathy
+(5,1,2),(5,2,3),(5,3,4),(5,4,4),(5,5,4),(5,6,3),(5,7,5),(5,8,3),(5,9,3),(5,10,5),(5,11,4),(5,12,3),(5,13,4),(5,14,3),(5,15,5),(5,16,4),(5,17,3),
+-- 6 The Wise Owl
+(6,1,1),(6,2,1),(6,3,4),(6,4,5),(6,5,3),(6,6,3),(6,7,3),(6,8,3),(6,9,2),(6,10,3),(6,11,5),(6,12,2),(6,13,4),(6,14,3),(6,15,2),(6,16,5),(6,17,1),
+-- 7 Creative Genius
+(7,1,2),(7,2,3),(7,3,4),(7,4,3),(7,5,3),(7,6,1),(7,7,3),(7,8,4),(7,9,2),(7,10,3),(7,11,4),(7,12,5),(7,13,3),(7,14,2),(7,15,2),(7,16,4),(7,17,5),
+-- 8 The Gentle Giant
+(8,1,5),(8,2,2),(8,3,3),(8,4,4),(8,5,4),(8,6,4),(8,7,4),(8,8,4),(8,9,4),(8,10,3),(8,11,3),(8,12,2),(8,13,4),(8,14,4),(8,15,5),(8,16,4),(8,17,1),
+-- 9 Lady Loyalty
+(9,1,2),(9,2,2),(9,3,4),(9,4,4),(9,5,4),(9,6,3),(9,7,4),(9,8,5),(9,9,3),(9,10,3),(9,11,3),(9,12,3),(9,13,3),(9,14,3),(9,15,3),(9,16,4),(9,17,3),
+-- 10 The Peacekeeper
+(10,1,2),(10,2,3),(10,3,4),(10,4,5),(10,5,4),(10,6,3),(10,7,4),(10,8,3),(10,9,3),(10,10,4),(10,11,4),(10,12,2),(10,13,5),(10,14,3),(10,15,4),(10,16,5),(10,17,2),
+-- 11 The Generous Guardian
+(11,1,2),(11,2,2),(11,3,4),(11,4,4),(11,5,4),(11,6,3),(11,7,3),(11,8,3),(11,9,5),(11,10,3),(11,11,4),(11,12,3),(11,13,3),(11,14,5),(11,15,3),(11,16,4),(11,17,3),
+-- 12 The Courageous Cat
+(12,1,4),(12,2,4),(12,3,4),(12,4,3),(12,5,3),(12,6,5),(12,7,3),(12,8,3),(12,9,3),(12,10,3),(12,11,3),(12,12,3),(12,13,3),(12,14,2),(12,15,2),(12,16,4),(12,17,3),
+-- 13 The Swift Swimmer
+(13,1,3),(13,2,5),(13,3,4),(13,4,3),(13,5,3),(13,6,4),(13,7,3),(13,8,3),(13,9,3),(13,10,4),(13,11,3),(13,12,3),(13,13,3),(13,14,2),(13,15,2),(13,16,4),(13,17,4),
+-- 14 The Kind-hearted Knight
+(14,1,3),(14,2,2),(14,3,4),(14,4,4),(14,5,5),(14,6,3),(14,7,5),(14,8,4),(14,9,3),(14,10,4),(14,11,4),(14,12,2),(14,13,3),(14,14,4),(14,15,4),(14,16,4),(14,17,2),
+-- 15 The Empathetic Eagle
+(15,1,3),(15,2,3),(15,3,4),(15,4,3),(15,5,4),(15,6,4),(15,7,5),(15,8,3),(15,9,3),(15,10,5),(15,11,4),(15,12,4),(15,13,3),(15,14,3),(15,15,4),(15,16,4),(15,17,5),
+-- 16 The Wise Wizard
+(16,1,1),(16,2,2),(16,3,4),(16,4,5),(16,5,3),(16,6,3),(16,7,3),(16,8,2),(16,9,2),(16,10,3),(16,11,5),(16,12,3),(16,13,3),(16,14,2),(16,15,2),(16,16,5),(16,17,1);
 
 CREATE EXTENSION IF NOT EXISTS vector;
 CREATE EXTENSION IF NOT EXISTS hstore;
